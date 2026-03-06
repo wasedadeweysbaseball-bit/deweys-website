589894ef5356e9648651503be2b8ad716f902b9e
岡大悟 <okadaigo@Mac.lan>
2026-03-06 14:43:12 +0900

    Add Google Search Console verification

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,10 @@ export const metadata = {
   title: '早稲田大学軟式野球サークル W.DEWEYS（デウェイズ）公式サイト',
   description: '早稲田大学を中心に活動する軟式野球サークルW.DEWEYS（デウェイズ）の公式サイトです。初心者・経験者大歓迎！練習日程や活動記録、入会案内を掲載中。',
   keywords: '早稲田大学, 野球サークル, 軟式野球, インカレ, デウェイズ, DEWEYS',
+  // Google Search Consoleの認証コードを追加
+  verification: {
+    google: 'G3NocVoas0iHsQtJlTiL6m__xiM5a9csHl0b6gJbHdQ',
+  },
 };
 
 export default function RootLayout({ children }) {
